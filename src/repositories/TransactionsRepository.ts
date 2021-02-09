cc38ceaf36b6429b8a38906c182e826ef9242a56
ewerton-vicentin <ewertonalex@hotmail.com>
2021-02-08 23:53:33 -0300

    Resultado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,11 +1,15 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
   total: number;
 }
-
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +18,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce(
+      (acummulator: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case "income":
+          acummulator.income += transaction.value;
+          break;
+        case "outcome":
+          acummulator.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return acummulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
+    const total = income - outcome;
+
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
